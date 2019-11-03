d8cbc9b26762efade61ec90e94f90ab015ee0bc1
Unai Herran <unai.herran@gmail.com>
2019-11-03 23:17:54 +0100

    first commit

---
+++ monitoring.py
@@ -0,0 +1,41 @@
+from influxdb import InfluxDBClient
+from datetime import datetime
+from secret import *
+import settings
+import glob
+
+client = InfluxDBClient(db_host, db_port, db_user, db_password, db_database)
+
+
+def define_json(measurement, time, fields):
+    #fields es un diccionario {"value": 0.64}
+    # time es datetime
+    # measurement es lo que se está midiendo
+    json_body = [
+        {
+            "measurement": [measurement],
+            "tags": {
+                "provider": "KC-BD-FF",
+            },
+            "time": [time.isoformat()],
+            "fields": fields
+
+        }
+    ]
+    return json_body
+
+
+def archivos_para_procesar():
+    directorio = settings.SCRAP_DIR + '*'
+    lista_archivos = glob.glob(directorio)
+    return len(lista_archivos)
+
+def main():
+    ahora = datetime.now()
+
+    json_body = define_json('Archivos en scrap',ahora, archivos_para_procesar())
+    client.write_points(json_body)
+    pass
+
+if __name__ == '__main__':
+    main()
---
+++ settings.py
@@ -0,0 +1,6 @@
+
+# Variables para el crawler
+headers = {'User-Agent': 'Crawler para Proyecto Estudiante Big Data - unai.herran@gmail.com'}
+
+#Directorios
+SCRAP_DIR = '/captura-camaras/scrapped/'
